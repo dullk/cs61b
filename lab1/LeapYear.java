55ff628521f6beb44b82c51a93d1b65bf4e9651b
Zhen Kang <kangzhen9911@gmail.com>
2023-05-18 12:24:16 +0800

    finish lab1 with LeapYear.java from sp19

---
+++ lab1/LeapYear.java
@@ -0,0 +1,21 @@
+public class LeapYear {
+
+    public static boolean isLeapYear (int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else {
+            return year % 4 == 0 && year % 100 != 0;
+        }
+
+
+
+    }
+    public static void main (String[] args) {
+        int year = Integer.parseInt(args[0]);
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
