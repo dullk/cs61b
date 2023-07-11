ee54fee849b558a20f97ec38b0ffc0646afc6dac
Zhen Kang <kangzhen9911@gmail.com>
2023-07-11 15:41:51 +0800

    back to the origin solution of Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,9 +1,14 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/** Class that print the Collatz sequence starting from a given number.
+ *  updated on May 3rd, 2023
+ *  @author Zhen Kang
  */
 public class Collatz {
-
-    /** Buggy implementation of nextNumber! */
+    /** Method that get the next term of a positive integer
+     *
+     * @param n is a positive integer
+     * @return n / 2 (if n is even)
+     *         3 * n + 1 (if n is odd)
+     */
     public static int nextNumber(int n) {
         if (n % 2 == 0) {
             return n / 2;
@@ -13,13 +18,13 @@ public class Collatz {
     }
 
     public static void main(String[] args) {
-        int n = 5;
-        System.out.print(n + " ");
-        while (n != 1) {
-            n = nextNumber(n);
-            System.out.print(n + " ");
+        int x = 5;
+        System.out.print(x + " ");
+        while (x != 1){
+            x = nextNumber(x);
+            System.out.print(x + " ");
         }
         System.out.println();
     }
-}
 
+}
