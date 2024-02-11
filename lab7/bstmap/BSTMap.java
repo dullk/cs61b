0b1e9a2b683b34a8393e7920f2ea3cbf12510087
Zhen Kang <kangzhen9911@gmail.com>
2024-02-11 14:24:02 -0800

    Passed all tests except for extra tests

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -3,14 +3,22 @@ package bstmap;
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K, V> implements Map61B<K, V> {
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
   private BSTNode root;
+  private int size;
 
   private class BSTNode {
     private K key;
     private V value;
     private BSTNode left;
     private BSTNode right;
+
+    private BSTNode(K key, V value) {
+      this.key = key;
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
 
     private K getKey() {
       return key;
@@ -27,30 +35,93 @@ public class BSTMap<K, V> implements Map61B<K, V> {
     private BSTNode getRight() {
       return right;
     }
+
+    private void setValue(V value) {
+      this.value = value;
+    }
+
+    private void setLeft (BSTNode node) {
+      left = node;
+    }
+
+    private void setRight (BSTNode node) {
+      right = node;
+    }
   }
+
+  public BSTMap() {
+    root = null;
+    size = 0;
+  }
+
   @Override
   public void clear() {
-
+    root = null;
+    size = 0;
   }
 
   @Override
   public boolean containsKey(K key) {
+    BSTNode node = root;
+    while (node != null) {
+      if (node.getKey().compareTo(key) == 0) {
+        return true;
+      } else if (node.getKey().compareTo(key) < 0) {
+        node = node.getRight();
+      } else if (node.getKey().compareTo(key) > 0) {
+        node = node.getLeft();
+      }
+    }
     return false;
   }
 
   @Override
   public V get(K key) {
+    BSTNode node = root;
+    while (node != null) {
+      if (node.getKey().compareTo(key) == 0) {
+        return node.getValue();
+      } else if (node.getKey().compareTo(key) < 0) {
+        node = node.getRight();
+      } else if (node.getKey().compareTo(key) > 0) {
+        node = node.getLeft();
+      }
+    }
     return null;
   }
 
   @Override
   public int size() {
-    return 0;
+    return size;
   }
 
   @Override
   public void put(K key, V value) {
-
+    BSTNode node = root;
+    BSTNode prev = null;
+    BSTNode newNode = new BSTNode(key, value);
+    if (node == null) {
+      root = newNode;
+      size += 1;
+      return;
+    }
+    while (node != null) {
+      prev = node;
+      if (node.getKey().compareTo(key) == 0) {
+        node.setValue(value);
+        return;
+      } else if (node.getKey().compareTo(key) < 0) {
+        node = node.getRight();
+      } else if (node.getKey().compareTo(key) > 0) {
+        node = node.getLeft();
+      }
+    }
+    if (prev.getKey().compareTo(key) > 0) {
+      prev.setLeft(newNode);
+    } else if (prev.getKey().compareTo(key) < 0) {
+      prev.setRight(newNode);
+    }
+    size += 1;
   }
 
   @Override
